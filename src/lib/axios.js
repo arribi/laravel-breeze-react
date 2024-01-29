0a06d77215a6e449aaf372e9a91e5d14a626b176
Jesús Arribi <arribi@boxer.local>
2024-01-29 14:43:19 +0000

    CSRF-TOKEN added

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -4,6 +4,7 @@ const axios = Axios.create({
   baseURL: 'https://arribi.informaticamajada.es',
   headers: {
     'X-Requested-With': 'XMLHttpRequest',
+    'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
   },
   withCredentials: true
 })
