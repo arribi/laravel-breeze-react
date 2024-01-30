6b9fe70bb65592632ae130ffa2a2cd139a4a95e2
Jesús Arribi <arribi@boxer.local>
2024-01-30 10:09:23 +0000

    axios fix

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -4,6 +4,7 @@ const axios = Axios.create({
   baseURL: 'https://arribi.informaticamajada.es',
   headers: {
     'X-Requested-With': 'XMLHttpRequest',
+    'X-XSRF-TOKEN': localStorage.getItem('XSRF-TOKEN'),
   },
   withCredentials: true,
   // withXSRFToken: true,
